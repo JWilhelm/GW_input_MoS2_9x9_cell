7a26081595e9d40757f406ba9218bad5185eec82
JWilhelm <jan.wilhelm@ur.de>
2024-06-05 10:35:46 +0200

    use updated version of GW code

--- 1_old/run_noctua.sh
+++ 1_old/run_noctua.sh
@@ -12,4 +12,4 @@ export OMP_PROC_BIND=true
 
 module reset
 module load chem/CP2K/2023.1-foss-2022b-gcc-openmpi-openblas
-srun  /pc2/groups/hpc-prf-eprop2d/eprop2d1_Jan/02_compile_CP2K/40*/cp2k/exe/local_with_convention_checks/cp2k.pdbg2    *.inp &> cp2k.out
+srun  /pc2/groups/hpc-prf-eprop2d/eprop2d1_Jan/02_compile_CP2K/20*/cp2k/exe/local/cp2k.psmp    *.inp &> cp2k.out
